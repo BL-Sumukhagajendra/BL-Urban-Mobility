bae2c7cbeed4d97afda6ca3a6ebb10c45a73c81f
sumukhamgajendra <sumukha.gajendra@bridgelabz.com>
2026-06-27 23:34:34 +0530

    feat: Added encalsulation feature along with testcases

---
+++ app/Test/test_vehicle.py
@@ -0,0 +1,47 @@
+from app.models.vehicle import Vehicle
+import pytest
+
+@pytest.fixture
+def vehicle():
+    return Vehicle(1, "Tesla")
+
+class TestVehicle:
+    def test_vehicle_id(self, vehicle):
+        assert vehicle.vehicle_id == 1
+
+    def test_model(self, vehicle):
+        assert vehicle.model == "Tesla"
+
+    def test_default_value_for_battery(self, vehicle):
+        assert vehicle.battery_percentage == 0  
+
+    def test_battery_percentage_setter(self, vehicle):
+        vehicle.set_battery_percentage = 50
+        assert vehicle.battery_percentage == 50
+ 
+    @pytest.mark.parametrize("battery", [1, 0, 99, 100])
+    def test_boundary_values_battery_percentage(self, battery,  vehicle):
+        vehicle.set_battery_percentage = battery
+        assert vehicle.battery_percentage == battery
+
+    @pytest.mark.parametrize("battery", [-1, 101])
+    def test_negative_boundary_values_battery_percentage(self, battery, vehicle):
+        with pytest.raises(ValueError):
+            vehicle.set_battery_percentage = battery
+
+    def test_negative_battery(self, vehicle):
+        with pytest.raises(ValueError):
+            vehicle.set_battery_percentage = -10
+
+    def test_default_values_for_mainatin_status(self, vehicle):
+        assert vehicle.mainatin_status == "AVAILABLE"        
+
+    def test_maintan_setter(self, vehicle):
+        vehicle.set_maintain_status = "UNAVAILABLE"
+        assert vehicle.mainatin_status == "UNAVAILABLE"
+
+    def test_maintain_status_wrong_input(self, vehicle):
+        with pytest.raises(ValueError):
+            vehicle.set_maintain_status = "ABCD"
+
+
--- app/models/vehicle.py
+++ app/models/vehicle.py
@@ -1,6 +1,30 @@
 class Vehicle:
-    def __init__(self, vehicle_id: int, model: str, battery_percentage: int):
+    def __init__(self, vehicle_id: int, model: str):
         self.vehicle_id = vehicle_id
         self.model = model
-        self.battery_percentage = battery_percentage
+        self.__battery_percentage = 0
+        self.__maitainence_status = "AVAILABLE"
 
+    @property
+    def battery_percentage(self):
+        return self.__battery_percentage
+    
+    @battery_percentage.setter
+    def set_battery_percentage(self, battery):
+        if battery < 0 or battery > 100:
+            raise ValueError("Battery shpuld be between 0 to 100 ")
+        
+        self.__battery_percentage = battery
+
+    @property
+    def mainatin_status(self):
+        return self.__maitainence_status
+    
+    @mainatin_status.setter
+    def set_maintain_status(self, status):
+        if status not in ["AVAILABLE", "UNAVAILABLE"]:
+            raise ValueError("Invalid status")
+
+        self.__maitainence_status = status
+        
+    
